5808fe3a72cc05dfa4950a9da77d24c70d25db4f
AgentRittik <109760373+AgentRittik@users.noreply.github.com>
2023-09-12 11:51:31 +0530

    Added v1 for tweet model and hashtag model

--- src/models/tweet.js
+++ src/models/tweet.js
@@ -5,27 +5,26 @@ const tweetSchema = new mongoose.Schema({
     content: {
         type: String,
         required : true,
+        max:[250, 'Tweet cannot be more than 250 characters']
     },
-    userEmail:{
-        type:String
-    },
-    comments:[
+    hashtags:[
         {
-          type: mongoose.Schema.Types.ObjectId,
-          ref:'Comment'
+            type:mongoose.Schema.Types.ObjectId,
+            ref: 'Hashtag'
         }
     ]
+
 },{timestamps: true});
 
-tweetSchema.virtual('contentWithEmail').get(function process(){
-    return `${this.content} \nCreated By : ${this.userEmail}`;
-});
-//triggers ad hooks 
-tweetSchema.pre('save', function(next){
-    console.log("inside a hook");
-    this.content = this.content+"....";  // manpulating the data that is coming from the user
-    next();
-})
+// tweetSchema.virtual('contentWithEmail').get(function process(){
+//     return `${this.content} \nCreated By : ${this.userEmail}`;
+// });
+// //triggers ad hooks 
+// tweetSchema.pre('save', function(next){
+//     console.log("inside a hook");
+//     this.content = this.content+"....";  // manpulating the data that is coming from the user
+//     next();
+// })
 
 
 
