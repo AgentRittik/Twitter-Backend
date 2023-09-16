bb9d8acb1d79a0f669871ad8bfba2a6f23ffc1b7
AgentRittik <109760373+AgentRittik@users.noreply.github.com>
2023-09-16 23:35:50 +0530

    Added like model and basic user model

---
+++ src/models/like.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+//very important concepts of refPath has been used 
+// if one action can performed on multiple things then we can follow this type of design
+const likeSchema = new mongoose.Schema({
+    onModel: {
+        type : String,
+        required : true,
+        enum : ['Tweet' , 'Comment']
+    },
+    likable : {
+        type : mongoose.Schema.Types.ObjectId,
+        required : true,
+        refPath : 'onModel'
+    },
+    user: {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : 'User',
+        required : true
+    }
+},{timestamps : true});
+
+const Like = mongoose.model('Like', likeSchema);
+
+export default Like;
